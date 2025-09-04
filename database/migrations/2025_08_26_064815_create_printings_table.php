e03aab137d1b6e7015a0a84e4928753a2a2b7516
laluisari-at <=>
2025-09-04 15:10:05 +0800

    feat: perubahan table printing

--- database/migrations/2025_08_26_064815_create_printings_table.php
+++ database/migrations/2025_08_26_064815_create_printings_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('printings', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_layanan');
+            $table->decimal('biaya');
+            $table->string('hitungan');
             $table->timestamps();
         });
     }
