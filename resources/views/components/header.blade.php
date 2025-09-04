ef1ace31c2303ec27dad7ddb1775ce615904ff36
laluisari-at <=>
2025-09-04 11:46:01 +0800

    style: perubahan pada tombol logout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,7 +6,10 @@
         <div class="flex absolute top-0 right-0">
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
-                <button type="submit" class="border border-red-600 px-2 py-1 rounded text-red-600 mt-2 mr-1 hover:bg-red-600 hover:text-white ">Logout</button>
+                <button type="submit"
+                    class="border border-red-600 px-2 py-1 rounded text-red-600 mt-2 mr-1
+                     hover:bg-red-600 hover:text-white active:scale-95">
+                    Logout</button>
             </form>
         </div>
     </div>
