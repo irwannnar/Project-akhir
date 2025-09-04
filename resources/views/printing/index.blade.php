88114f0f02d3991e93c9b6126ff3016e37940d4c
laluisari-at <=>
2025-09-04 14:40:41 +0800

    feat: mengubah tampilan halaman printing

--- resources/views/printing/index.blade.php
+++ resources/views/printing/index.blade.php
@@ -1,22 +1,14 @@
 <x-layout.default>
     <div class="containermx-auto p-3">
-        <div class="bg-white rounded">
+        <div class="bg-white rounded flex items-center justify-center">
             <div class="grid grid-cols-1 lg:grid-cols-4 mb-4 p-3 gap-6">
-                <div class="lg:col-span-1 bg-blue-100 rounded">
-                    <h4>Digital</h4>
-                    <div class="h-40"></div>
-                </div>
-                <div class="lg:col-span-1 bg-red-100 rounded">
-                    <h4>Sablon</h4>
-                    <div class="h-40"></div>
-                </div>
-                <div class="lg:col-span-1 bg-green-100 rounded">
-                    <h4>offset</h4>
-                    <div class="h-40"></div>
-                </div>
-                <div class="lg:col-span-1 bg-yellow-100 rounded">
-                    <h4>Sublimasi</h4>
-                    <div class="h-40"></div>
+                <div class="bg-blue-100 rounded">
+                    <label for="printing" name="printing" id="printing">Printing</label>
+                    <select name="service" id="service">
+                        @foreach($printingService as $key => $service)
+                            <option value="{{ $printingService->id }}"></option>
+                        @endforeach
+                    </select>
                 </div>
             </div>
         </div>
