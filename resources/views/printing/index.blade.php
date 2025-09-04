0fc14d00d2437aa4c90442859e52ec2e4ded8b74
laluisari-at <=>
2025-09-04 16:46:36 +0800

    feat: menambahkan beberapa fitur -tabel layanan beserta crud -button ke halaman create

--- resources/views/printing/index.blade.php
+++ resources/views/printing/index.blade.php
@@ -1,15 +1,49 @@
 <x-layout.default>
-    <div class="containermx-auto p-3">
-        <div class="bg-white rounded flex items-center justify-center">
-            <div class="grid grid-cols-1 lg:grid-cols-4 mb-4 p-3 gap-6">
-                <div class="bg-blue-100 rounded">
-                    <label for="printing" name="printing" id="printing">Printing</label>
-                    <select name="service" id="service">
-                        @foreach($service as $key => $layanan)
-                            <option value="{{  }}"></option>
+    <div class="containermx-auto px-4 py-6">
+        <div class="flex flex-col sm:flex-row items-center justify-between mb-6 gap-6">
+            <div>
+                <h1>jasa Printing</h1>
+                <a href="/"
+                    class="border border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white px-1 py-1 rounded hover:animate-bounce active:scale-95">Tambah
+                    Layanan</a>
+            </div>
+        </div>
+        <div>
+            <div class="bg-white shadow-lg rounded-xl overflow-hidden">
+                <div class=""></div>
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th class="px-6 py-5 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
+                                Layanan</th>
+                            <th class="px-6 py-5 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
+                                Biaya</th>
+                            <th class="px-6 py-5 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
+                                Perhitungan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($printing as $key => $layanan)
+                            <tr>
+                                <td class="px-6 py-5 text-sm font-medium text-gray-600">{{ $layanan->nama_layanan }}
+                                </td>
+                                <td class="px-6 py-5 text-sm font-medium text-gray-600">{{ $layanan->biaya }}</td>
+                                <td class="px-6 py-5 text-sm font-medium text-gray-600">{{ $layanan->hitungan }}</td>
+                                <td class="px-6 py-5 text-sm font-medium text-gray-600">
+                                    <div>
+                                        <a href="/">Edit</a>
+                                        <form action="" method="POST" class="inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" onclick="return confirm('yakin?')"
+                                                class="inline bg-red-600 text-white active:scale-95 rounded hover:bg-red-800 transition duration-200">Hapus</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
                         @endforeach
-                    </select>
-                </div>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
