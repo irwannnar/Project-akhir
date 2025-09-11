6508cec64bdb9c74480c20b037b4e2afcddee43e
laluisari-at <=>
2025-09-11 10:15:10 +0800

    fix: nama pelanggan tidak muncul di halaman edit order

--- resources/views/order/edit.blade.php
+++ resources/views/order/edit.blade.php
@@ -38,7 +38,6 @@
                             class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                             placeholder="Masukkan nama pelanggan"
                             required
-                            x-model="customerName"
                             value="{{ old('customer_name', $order->customer_name) }}"
                         >
                     </div>
