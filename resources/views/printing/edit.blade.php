d63d33ece34be55c297e538f8cc10430aa3c7fa5
laluisari-at <=>
2025-09-29 16:41:04 +0800

    refactor: menghapus data ukuran pada edit printing

--- resources/views/printing/edit.blade.php
+++ resources/views/printing/edit.blade.php
@@ -12,36 +12,27 @@
                         <label for="nama_layanan">Nama layanan</label>
                         <input type="text" name="nama_layanan" id="nama_layanan" placeholder="nama layanan"
                             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            value="{{ $printing->nama_layanan }}"
-                            required>
+                            value="{{ $printing->nama_layanan }}" required>
                     </div>
                     <div>
                         <label for="biaya">biaya</label>
                         <input type="number" name="biaya" id="biaya" placeholder="biaya"
                             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            value="{{ $printing->biaya }}"
-                            required>
+                            value="{{ $printing->biaya }}" required>
                     </div>
                 </div>
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-3 mt-2">
-                    <div>
-                        <label for="biaya" class="font-bold">Ukuran</label>
-                        <input type="text" name="ukuran" id="ukuran" placeholder="ukuran"
-                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            value="{{ $printing->ukuran }}"
-                            required>
-                    </div>
-                    <div>
+                <div>
                     <label for="hitungan">Perhitungan</label>
                     <input type="text" name="hitungan" id="hitungan" placeholder="hitungan"
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        value="{{ $printing->hitungan }}"
-                        required>
-                    </div>
+                        value="{{ $printing->hitungan }}" required>
                 </div>
                 <div class="flex justify-between mt-8">
-                    <a href="{{ route('printing.index') }}" class="border border-gray-600 bg-gray-600 hover:bg-gray-800 text-white px-2 py-1 rounded active:scale-95">batal</a>
-                    <button type="submit" class="border border-blue-600 bg-blue-600 hover:bg-blue-800 text-white px-2 py-1 rounded active:scale-95">Simpan perubahan</button>
+                    <a href="{{ route('printing.index') }}"
+                        class="border border-gray-600 bg-gray-600 hover:bg-gray-800 text-white px-2 py-1 rounded active:scale-95">batal</a>
+                    <button type="submit"
+                        class="border border-blue-600 bg-blue-600 hover:bg-blue-800 text-white px-2 py-1 rounded active:scale-95">Simpan
+                        perubahan</button>
                 </div>
             </form>
         </div>
