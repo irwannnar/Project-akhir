cf87d45eef8a53ec965a0073dc52f81a0c85d224
laluisari-at <=>
2025-10-07 10:23:34 +0800

    fix: perubahan pada statistik pesanan -statistik pesanan jadi statistik pesanan layanan dan produk

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -90,7 +90,7 @@
                                 d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z">
                             </path>
                         </svg>
-                        Statistik Pesanan
+                        Statistik Pesanan Layanan dan Produk
                     </h3>
 
                     <div class="space-y-4">
