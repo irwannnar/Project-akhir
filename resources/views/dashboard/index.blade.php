dea95cb62abe8a92bbcb0a47356a1b08cab4780e
laluisari-at <=>
2025-10-09 11:14:18 +0800

    style: sedikit perubahan warna pada donut chart

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -362,7 +362,7 @@
                         // Warna yang berbeda untuk setiap layanan
                         const backgroundColors = [
                             '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF',
-                            '#FF9F40', '#FF6384', '#C9CBCF', '#4BC0C0', '#36A2EB',
+                            '#FF9F40', '#126654', '#C9CBCF', '#4BC0C0', '#36A2EB',
                             '#FF6384', '#FFCE56', '#9966FF', '#FF9F40', '#C9CBCF'
                         ];
 
